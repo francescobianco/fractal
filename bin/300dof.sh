0953151a0d5d15dd70cfb8f8a20e994d4a5d27e0
francescobianco <info.francescobianco@gmail.com>
2021-03-05 20:22:17 +0100

    first commit

--- bin/300dof.sh
+++ bin/300dof.sh
@@ -20,8 +20,8 @@ offset=11113
 echo -e "# 300 Days of Fractal\n" > 300DOF.md
 while IFS="" read -r url || [ -n "${url}" ]; do
   post_date=$(date --date "${start_date} ${days} day" +'%a %d/%m/%Y' | tr '[a-z]' '[A-Z]')
-  tweet="${url}"
-  intent="https://twitter.com/intent/tweet?text=${tweet}"
+  tweet="Fractal"
+  intent="https://twitter.com/intent/tweet?text=${tweet}&url=${url}"
   echo "- \`${post_date} D${day}\` [✍](${intent})" >> 300DOF.md
   offset="${offset:1:5}${offset:0:1}"
   days=$((${days} + ${offset:0:1}))
