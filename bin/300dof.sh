f36dbb668d424206cafe29f4ff14261c4bfb43b9
francescobianco <info.francescobianco@gmail.com>
2021-03-05 20:12:23 +0100

    first commit

--- bin/300dof.sh
+++ bin/300dof.sh
@@ -1,19 +1,27 @@
 #!/usr/bin/env bash
 
-echo "Sourcing..."
-rm -f etc/fractal.list && true
-while IFS="" read -r url || [ -n "${url}" ]; do
-  wget -kqO index.html "${url}"
-  cat index.html | grep img | grep -Po 'src="\K.*?(?=")' >> etc/fractal.list
-  rm -f index.html
-done < etc/sources.list
+start_date="2021-03-01"
 
-echo "Pruning images..."
-sed -i '/\.\(png\|jpg\|jpeg\)$/I!d' etc/fractal.list
-sort -R --random-source etc/random etc/fractal.list
+#echo "Sourcing..."
+#rm -f etc/fractal.list && true
+#while IFS="" read -r url || [ -n "${url}" ]; do
+#  wget -kqO index.html "${url}"
+#  cat index.html | grep img | grep -Po 'src="\K.*?(?=")' >> etc/fractal.list
+#  rm -f index.html
+#done < etc/sources.list
+#
+#echo "Pruning images..."
+#sed -i '/\.\(png\|jpg\|jpeg\)$/I!d' etc/fractal.list
+#sort -R --random-source etc/random etc/fractal.list
 
+day=1
+days=0
+offset=11113
 echo -e "# 300 Days of Fractal\n" > 300DOF.md
 while IFS="" read -r url || [ -n "${url}" ]; do
-  echo "- [✍](${url})"  >> 300DOF.md
+  post_date=$(date --date "${start_date} ${days} day" +'%a %d/%m/%Y' | tr '[a-z]' '[A-Z]')
+  echo "- ${post_date} D${day} [✍](${url})" >> 300DOF.md
+  offset="${offset:1:5}${offset:0:1}"
+  days=$((${days} + ${offset:0:1}))
+  day=$((${day} + 1))
 done < etc/fractal.list
-
---
+++ bin/str.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+offset=11113
+
+offset="${offset:1:5}${offset:0:1}"
+
+echo "${b}"
+
