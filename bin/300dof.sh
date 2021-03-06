bf225f64eb0154745fe0f6af0ffc6e1a1e6f4f4d
francescobianco <info.francescobianco@gmail.com>
2021-03-06 01:06:22 +0100

    first commit

--- bin/300dof.sh
+++ bin/300dof.sh
@@ -21,12 +21,14 @@ echo -e "# 300 Days of Fractal\n" > 300DOF.md
 while IFS="" read -r fractal_url || [ -n "${fractal_url}" ]; do
   post_date=$(date --date "${start_date} ${days} day" +'%a %d/%m/%Y' | tr '[a-z]' '[A-Z]')
   tweet="Fractal"
-  intent="https://twitter.com/intent/tweet?text=${tweet}&url=${url}"
+  domain="$(echo ${fractal_url} | sed -e 's/[^/]*\/\/\([^@]*@\)\?\([^:/]*\).*/\2/')"
+  tweet_url="https://francescobianco.github.io/fractal/post/300-days-of-fractal/day-${day}.html"
+  intent="https://twitter.com/intent/tweet?text=${tweet}&url=${tweet_url}"
   echo "- \`${post_date} D${day}\` [✍](${intent})" >> 300DOF.md
   sed -e 's!{fractal_url}!'"${fractal_url}"'!' \
-    -e 's!%%VT_DOWNLOAD%%!'"${download}"'!' \
-    -e 's!%%VT_DIRECTORY%%!'"${directory}"'!' \
-    -e 's!%%DATABASE_PACKAGE%%!'"${database_package}"'!' \
+    -e 's!{day}!'"${day}"'!' \
+    -e 's!{tweet_url}!'"${tweet_url}"'!' \
+    -e 's!{domain}!'"${domain}"'!' \
     -e 's!%%PHP_VERSION%%!'"${php_version}"'!' \
     -r etc/300dof.tpl > post/300-days-of-fractal/day-${day}.html
   offset="${offset:1:5}${offset:0:1}"
